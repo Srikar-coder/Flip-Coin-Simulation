8769aac3de570873927ae31594c67299ecaac93c
Srikar <srikarsamanchi1999@gmail.com>
2021-05-16 15:42:52 +0530

    Added: Tie Breaker

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -20,6 +20,25 @@ done
 if [ $Heads -eq $Tails ]
 then
 echo It is a Tie
+while [ 1 -eq 1 ]
+do
+toss2=$((RANDOM%2))
+if [ $toss2 -eq 0 ]
+then
+((Heads++))
+else
+((Tails++))
+fi
+if [ $(($Heads-$Tails)) -gt 2 ]
+then
+echo Heads
+exit 0
+elif [ $(($Tails-$Heads)) -gt 2 ]
+then
+echo Tails
+exit 0
+fi
+done
 elif [ $Heads -gt $Tails ]
 then
 echo Heads won by $(($Heads-$Tails)) ponits
