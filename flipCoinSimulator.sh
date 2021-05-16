1887f11edebf1cd7de378a18ae866efd05e07a06
Srikar <srikarsamanchi1999@gmail.com>
2021-05-16 15:16:26 +0530

    Added: number of tails and heads

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -x
 
+for ((var=1; var<=10; var++))
+do
 toss=$((RANDOM%2))
 
 if [ $toss -eq 0 ]
 then
-echo Heads
+((Heads++))
 else
-echo Tails
+((Tails++))
 fi
+done
+echo number of heads $Heads
+echo number of tails $Tails
